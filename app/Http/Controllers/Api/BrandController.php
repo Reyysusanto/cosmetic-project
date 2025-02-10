5735fbd8daf5276cba1d91a01c9dd99521133a49
Reyysusanto <rheinaldysusanto@gmail.com>
2025-02-10 21:32:02 +0700

    brand and category controller

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\BrandApiResource;
+use App\Models\Brand;
 
 class BrandController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $brands = Brand::withCount(['cosmetics']);
+
+        if($request->has('limit')) {
+            $brands->limit($request->input('limit'));
+        }
+
+        return BrandApiResource::collection($brands->get());
+    }
+
+    public function show (Brand $brand)
+    {
+        $brand->load(['cosmetics', 'popularCosmetics']);
+        $brand->loadCount(['cosmetics']);
+
+        return new BrandApiResource($brand);
+    }
 }
