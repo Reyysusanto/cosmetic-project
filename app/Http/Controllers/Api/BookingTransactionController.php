60a055009522ce6cfbad61237b3f4aabd2139a1b
Reyysusanto <rheinaldysusanto@gmail.com>
2025-02-11 09:02:14 +0700

    store booking transaction

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,63 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Http\Resources\Api\BookingTransactionApiResource;
+use App\Models\BookingTransaction;
+use App\Models\Cosmetic;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        try {
+            
+            $validatedData = $request->validated();
+
+            if($request->hasFile('proof')) {
+                $filePath = $request->file('proof')->store('proofs', 'public');
+                $validatedData['proof'] = $filePath;
+            }
+
+            $products = $request->input('cosmetic_ids');
+            $totalQuantity = 0;
+            $totalPrice = 0;
+
+            $cosmeticIds = array_column($products, 'id');
+            $cosmetics = Cosmetic::whereIn('id', $cosmeticIds)->get();
+
+            foreach($products as $product) {
+                $cosmetic = $cosmetics->firstWhere('id', $product['id']);
+                $totalQuantity += $product['quantity'];
+                $totalPrice += $cosmetic->price * $product['quantity'];
+            }
+
+            $tax = 0.11 * $totalPrice;
+            $grandPrice = $tax + $totalPrice;
+
+            $validatedData['total_amount'] = $grandPrice;
+            $validatedData['total_tax_amount'] = $tax;
+            $validatedData['total_price'] = $totalPrice;
+            $validatedData['is_paid'] = false;
+            $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueId();
+            $validatedData['quantity'] = $totalQuantity;
+
+            $bookingTransaction = BookingTransaction::create($validatedData);
+
+            foreach($products as $product) {
+                $cosmetic = $cosmetic->firstWhere('id', $product['id']);
+                $bookingTransaction->transactionDetails()->create([
+                    'cosmetic_id' => $product['id'],
+                    'quantity' => $product['quantity'],
+                    'price' => $cosmetic->price,
+                ]);
+            }
+
+            return new BookingTransactionApiResource($bookingTransaction->load('transactionDetails'));
+
+        } catch(\Exception $err){
+            return response()->json(['message' => 'An error occurred', 'error' => $err->getMessage()], 500);
+        }
+    }
 }
